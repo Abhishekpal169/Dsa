572b99aaf366062f91253c5da9759cb2599575eb
Abhishek Pal <abhishekpal2437@gmail.com>
2026-03-10 21:56:40 +0530

    dsa

---
+++ prime.java
@@ -0,0 +1,16 @@
+public class prime {
+    public static void main(String[]args){
+        int num = 23; 
+        boolean isPrime = true;
+        if(num<=1){
+            isPrime = false;
+        }
+        for(int i=2;i<=num/2;i++){
+            if(num %i == 0){
+                isPrime = false;
+            }
+
+        }
+        System.out.println(isPrime);
+    }
+}
