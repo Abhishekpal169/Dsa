e01a35e0492a594c61b0fb40b74fd49e0e7f9e2b
Abhishek Pal <abhishekpal2437@gmail.com>
2026-03-12 20:55:00 +0530

    cs

---
+++ problem13.java
@@ -0,0 +1,19 @@
+
+public class problem13 {
+    
+    public static void main(String[] args) {
+        int my_num;
+        int[] numbers = new int[]{1, 2, 3, 4, 6, 7};
+        my_num = 7;
+       int expected_num = my_num * ((my_num + 1) / 2);
+
+        int num_sum = 0;
+
+        for (int i : numbers) {
+            num_sum += i;
+        }
+        System.out.print(expected_num - num_sum);
+        System.out.print("\n");
+    }
+}
+
