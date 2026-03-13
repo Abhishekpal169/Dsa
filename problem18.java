bf068a84469fba5de2447e7d213768febc2fb5bf
Abhishek Pal <abhishekpal2437@gmail.com>
2026-03-13 20:28:30 +0530

    cs

---
+++ problem18.java
@@ -0,0 +1,19 @@
+
+    import java.util.HashMap;
+    public class problem18 {
+
+    public static void main(String[] args) {
+        String str = "abhishekai";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : str.toCharArray()) {
+            if (map.get(c) == 1) {
+                System.out.println(c);
+                break;
+            }
+        }
+    }
+}
+
