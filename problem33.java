bc4d1b020c75e733da87e411ee206cef0e2b6e49
Abhishek Pal <abhishekpal2437@gmail.com>
2026-03-18 22:58:50 +0530

    cs

---
+++ problem33.java
@@ -0,0 +1,31 @@
+
+    public class problem33 {
+
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {3, 2, 3};
+        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+
+        System.out.println(majorityElement(nums1)); 
+        System.out.println(majorityElement(nums2)); 
+    }
+}
+
