f3dc006566119a1765d0930c027f77f0a2ff9bcf
Abhishek Pal <abhishekpal2437@gmail.com>
2026-03-26 12:37:24 +0530

    cs

---
+++ problem30.java
@@ -0,0 +1,11 @@
+ public class problem30{
+    public static void main(String[] args) {
+     String s ="bhakti";
+     for(int i=0;i<s.length();i++){
+        for(int j=i+1;j<=s.length();j++){
+            System.out.print(s.substring(i,j) + " ");
+        }
+        System.out.println();
+     }
+    }
+} 
